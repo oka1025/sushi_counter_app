930e6b426ba9bc95e17ae0fafeb41d55b5dcf8f6
oka1025 <ocelot1025@yahoo.co.jp>
2025-05-30 14:23:31 +0900

    ユーザー認証機能実装#7

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,10 @@ require_relative "boot"
 require "rails/all"
 require "logger"
 
+
+
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,6 +15,10 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   root to: "home#index"
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
