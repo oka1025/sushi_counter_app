b277c3c0360b165ac63b89422e263aca56545a7d
Haruki Okamoto <ocelot1025@yahoo.co.jp>
2025-07-04 18:26:03 +0900

    スクロール位置修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -30,6 +30,8 @@ document.addEventListener("turbo:load", () => {
 document.addEventListener("turbo:before-fetch-request", () => {
   if (window.location.pathname.includes("/gachas/result")) return;
   if (window.location.pathname.includes("/user_gacha_lists")) return;
+
+  // 通常のスクロール位置保存
   const key = `scrollY:${location.pathname}`;
   sessionStorage.setItem(key, window.scrollY);
 });
@@ -37,9 +39,19 @@ document.addEventListener("turbo:before-fetch-request", () => {
 document.addEventListener("turbo:load", () => {
   if (window.location.pathname.includes("/gachas/result")) return;
   if (window.location.pathname.includes("/user_gacha_lists")) return;
+
+  const scrollAfter = sessionStorage.getItem("scrollAfterSearchY");
+
+  if (scrollAfter !== null) {
+    // 検索後の強制スクロールが指定されているとき
+    window.scrollTo({ top: parseInt(scrollAfter, 10), behavior: "smooth" });
+    sessionStorage.removeItem("scrollAfterSearchY");
+    return;
+  }
+
+  // 通常スクロール復元
   const key = `scrollY:${location.pathname}`;
   const y = sessionStorage.getItem(key);
-
   if (y !== null) {
     window.scrollTo(0, parseInt(y, 10));
     sessionStorage.removeItem(key);
