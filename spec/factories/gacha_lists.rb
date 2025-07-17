571a5737d780b576dadf41281393fd025129062e
Haruki Okamoto <ocelot1025@yahoo.co.jp>
2025-07-18 00:24:31 +0900

    gacha_spec#115

--- spec/factories/gacha_lists.rb
+++ spec/factories/gacha_lists.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :gacha_list do
     name { "まぐろガチャ" }
     rarity { "rare" }
+
+    after(:build) do |gacha|
+      gacha.image.attach(
+        io: File.open(Rails.root.join("spec/fixtures/files/sample.png")),
+        filename: "sample.png",
+        content_type: "image/png"
+      )
+    end
   end
 end
---
+++ spec/requests/gachas_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Gachas", type: :request do
+  let(:user) { create(:user, coin: 10) }
+
+  before { sign_in user }
+
+  describe "GET /gachas/:id" do
+    it "正しいビューに遷移する" do
+      gacha = create(:gacha_list)
+      get gachas_path(gacha)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /gachas/draw" do
+    context "コインが不足している場合" do
+      it "プレイできない" do
+        user.update(coin: 3)
+        post draw_gachas_path, params: { times: 1 }
+        expect(response).to redirect_to(gachas_path)
+        follow_redirect!
+        expect(response.body).to include("コインが足りません")
+      end
+    end
+
+    context "コインが十分にある場合" do
+      it "ガチャを引いてコインが減り、リダイレクトされる" do
+        expect {
+          post draw_gachas_path, params: { times: 1 }
+        }.to change(UserGachaList, :count).by(1)
+
+        expect(user.reload.coin).to eq(5)
+        expect(response).to redirect_to(result_gachas_path)
+      end
+    end
+  end
+
+  describe "GET /gachas/result" do
+    it "結果画面が表示される" do
+      get result_gachas_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
