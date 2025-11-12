ae13abb9f0c286dff119039f8a091b255c081edf
Haruki Okamoto <ocelot1025@yahoo.co.jp>
2025-11-12 22:10:43 +0900

    CDN修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,6 +106,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.resolve_model_to_route = :rails_storage_proxy
+  config.asset_host = "https://sushi-counter.imgix.net"
 
   config.hosts << 'www.sushi-counter.com'
   config.hosts << 'sushi-counter.com'
